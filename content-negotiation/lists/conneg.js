cf579e847e208b91de657e44dbaaa36c238e19c2
BigBlueHat <byoung@bigbluehat.com>
2016-01-19 08:54:13 -0500

    Wrapped output in a function; works on Cloudant now!

--- content-negotiation/lists/conneg.js
+++ content-negotiation/lists/conneg.js
@@ -1,17 +1,22 @@
 function(head, req) {
-  var output = [];
-  while (row = getRow()) {
-    output.push(row);
+  function getOutput() {
+    var output = [];
+    while (row = getRow()) {
+      output.push(row);
+    }
+    return output;
   }
 
   // Content Negotiation happens via the provides() function
   // http://docs.couchdb.org/en/1.6.1/query-server/javascript.html#provides
   provides('json', function() {
+    var output = getOutput();
     // default Content-Type is application/json, so we don't need start() here
     // but we will pretty print it ^_^
     return JSON.stringify(output, null, 4);
   });
   provides('html', function() {
+    var output = getOutput();
     // using start() to setup the proper Content-Type header
     // ...CouchDB was/should-be doing this automatically...
     start({
